f25b56edc250b1e1587f6bae047de6d82b43c0de
moddedTechnic <jpleeming51@gmail.com>
2020-08-03 16:16:45 +0100

    Added circle and stretched circle

--- src/window/tools/__init__.py
+++ src/window/tools/__init__.py
@@ -1,2 +1,2 @@
 from .text import draw_text, fonts
-from .shape import rounded_rect
+from .shape import rounded_rect, stretched_circle, circle
--- src/window/tools/shape.py
+++ src/window/tools/shape.py
@@ -9,6 +9,14 @@ from window.constants import constants
 Colour = constants.colour.Colour
 
 
+def circle(surface: Surface, x: int, y: int, r: int, colour):
+    x = int(x)
+    y = int(y)
+    r = int(r)
+
+    aacircle(surface, x, y, r, colour)
+    filled_circle(surface, x, y, r, colour)
+
 def rounded_rect(surface: Surface, rect: Rect, colour: Colour, corner_radius: int):
     ''' Draw a rectangle with rounded corners.
     Would prefer this: 
@@ -23,15 +31,10 @@ def rounded_rect(surface: Surface, rect: Rect, colour: Colour, corner_radius: in
     corner_radius = int(corner_radius)
 
     # need to use anti aliasing circle drawing routines to smooth the corners
-    aacircle(surface, rect.left  + corner_radius,     rect.top + corner_radius,        corner_radius, colour)
-    aacircle(surface, rect.right - corner_radius - 1, rect.top + corner_radius,        corner_radius, colour)
-    aacircle(surface, rect.left  + corner_radius,     rect.bottom - corner_radius - 1, corner_radius, colour)
-    aacircle(surface, rect.right - corner_radius - 1, rect.bottom - corner_radius - 1, corner_radius, colour)
-
-    filled_circle(surface, rect.left + corner_radius,      rect.top + corner_radius,        corner_radius, colour)
-    filled_circle(surface, rect.right - corner_radius - 1, rect.top + corner_radius,        corner_radius, colour)
-    filled_circle(surface, rect.left + corner_radius,      rect.bottom - corner_radius - 1, corner_radius, colour)
-    filled_circle(surface, rect.right - corner_radius - 1, rect.bottom - corner_radius - 1, corner_radius, colour)
+    circle(surface, rect.left  + corner_radius,     rect.top + corner_radius,        corner_radius, colour)
+    circle(surface, rect.right - corner_radius - 1, rect.top + corner_radius,        corner_radius, colour)
+    circle(surface, rect.left  + corner_radius,     rect.bottom - corner_radius - 1, corner_radius, colour)
+    circle(surface, rect.right - corner_radius - 1, rect.bottom - corner_radius - 1, corner_radius, colour)
 
     rect_tmp = Rect(rect)
 
@@ -43,3 +46,12 @@ def rounded_rect(surface: Surface, rect: Rect, colour: Colour, corner_radius: in
     rect_tmp.height -= 2 * corner_radius
     rect_tmp.center = rect.center
     draw_rect(surface, colour, rect_tmp)
+
+def stretched_circle(surface: Surface, rect: Rect, colour: Colour):
+    rounded_rect(surface, rect, colour, min(rect.width, rect.height) * .5)
+    r = rect.copy()
+    r.width -= r.height
+    r.x += r.height / 2
+    r.height += 2
+    r.y -= 1
+    draw_rect(surface, colour, r)
