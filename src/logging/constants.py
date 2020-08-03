3f81d0f73828ab51fd7dd311fe000b8171ce5ddf
moddedTechnic <jpleeming51@gmail.com>
2020-08-03 15:09:09 +0100

    Added more colours and styles

--- src/logging/constants.py
+++ src/logging/constants.py
@@ -1,24 +1,85 @@
+from typing import NewType
+
+def base(n: int) -> str:
+	return u'\u001b[' + str(n)
 
 class constants:
-	class colour:
-		BLACK:   str = u'\u001b[30m'
-		RED:     str = u'\u001b[31m'
-		GREEN:   str = u'\u001b[32m'
-		YELLOW:  str = u'\u001b[33m'
-		BLUE:    str = u'\u001b[34m'
-		MAGENTA: str = u'\u001b[35m'
-		CYAN:    str = u'\u001b[36m'
-		WHITE:   str = u'\u001b[37m'
+	class style:
+		class colour:
+			BLACK:   str = u'\u001b[30m'
+			RED:     str = u'\u001b[31m'
+			GREEN:   str = u'\u001b[32m'
+			YELLOW:  str = u'\u001b[33m'
+			BLUE:    str = u'\u001b[34m'
+			MAGENTA: str = u'\u001b[35m'
+			CYAN:    str = u'\u001b[36m'
+			WHITE:   str = u'\u001b[37m'
 
-		BRIGHT_BLACK:   str = u'\u001b[30;1m'
-		BRIGHT_RED:     str = u'\u001b[31;1m'
-		BRIGHT_GREEN:   str = u'\u001b[32;1m'
-		BRIGHT_YELLOW:  str = u'\u001b[33;1m'
-		BRIGHT_BLUE:    str = u'\u001b[34;1m'
-		BRIGHT_MAGENTA: str = u'\u001b[35;1m'
-		BRIGHT_CYAN:    str = u'\u001b[36;1m'
-		BRIGHT_WHITE:   str = u'\u001b[37;1m'
+			BRIGHT_BLACK:   str = u'\u001b[30;1m'
+			BRIGHT_RED:     str = u'\u001b[31;1m'
+			BRIGHT_GREEN:   str = u'\u001b[32;1m'
+			BRIGHT_YELLOW:  str = u'\u001b[33;1m'
+			BRIGHT_BLUE:    str = u'\u001b[34;1m'
+			BRIGHT_MAGENTA: str = u'\u001b[35;1m'
+			BRIGHT_CYAN:    str = u'\u001b[36;1m'
+			BRIGHT_WHITE:   str = u'\u001b[37;1m'
 
-		COLOURS_8 = [u'\u001b[38;5;' + str(x) + 'm' for x in range(256)]
+			COLOURS_8 = [u'\u001b[38;5;' + str(x) + 'm' for x in range(256)]
 
-		RESET:   str = u'\u001b[0m'
+			RESET:   str = u'\u001b[0m'
+
+		BOLD:      str = u'\u001b[1m'
+		UNDERLINE: str = u'\u001b[4m'
+		REVERSED:  str = u'\u001b[7m'
+
+	class cursor:
+		@classmethod
+		def up(cls, n: int) -> str:
+			return base(n) + 'A'
+
+		@classmethod
+		def down(cls, n: int) -> str:
+			return base(n) + 'B'
+
+		@classmethod
+		def right(cls, n: int) -> str:
+			return base(n) + 'C'
+
+		@classmethod
+		def left(cls, n: int) -> str:
+			return base(n) + 'D'
+
+	class clear:
+		mode = NewType('mode', int)
+
+		SCREEN_END:   mode = 0
+		SCREEN_START: mode = 1
+		SCREEN_WHOLE: mode = 2
+
+		@classmethod
+		def screen(cls, n: mode) -> str:
+			return base(n) + 'J'
+			
+		LINE_END:   mode = 0
+		LINE_START: mode = 1
+		LINE_WHOLE: mode = 2
+			
+		@classmethod
+		def line(cls, n: mode) -> str:
+			return base(n) + 'K'
+
+	@classmethod
+	def next_line(cls, n: int) -> str:
+		return base(n) + 'E'
+
+	@classmethod
+	def prev_line(cls, n: int) -> str:
+		return base(n) + 'F'
+	
+	@classmethod
+	def set_col(cls, n: int) -> str:
+		return base(n) + 'G'
+
+	@classmethod
+	def next_line(cls, n: int, m: int) -> str:
+		return base(n) + str(m) + 'H'
--- src/window/constants.py
+++ src/window/constants.py
@@ -1,20 +1,56 @@
 from typing import Tuple
 
+Colour = Tuple[int, int, int]
+
 class constants:
 	class colour:
-		BLACK:      Tuple[int, int, int] =   0,   0,   0
-		DARK_GREY:  Tuple[int, int, int] =  63,  63,  63
-		GREY:       Tuple[int, int, int] = 127, 127, 127
-		LIGHT_GREY: Tuple[int, int, int] = 191, 191, 191
-		WHITE:      Tuple[int, int, int] = 255, 255, 255
+		Colour = Colour
 
-		RED:   Tuple[int, int, int] = 255,   0,   0
-		GREEN: Tuple[int, int, int] =   0, 255,   0
-		BLUE:  Tuple[int, int, int] =   0,   0, 255
+		BLACK:      Colour =   0,   0,   0
+		DARK_GREY:  Colour =  63,  63,  63
+		GREY:       Colour = 127, 127, 127
+		LIGHT_GREY: Colour = 191, 191, 191
+		WHITE:      Colour = 255, 255, 255
 
-		CYAN:    Tuple[int, int, int] =   0, 255, 255
-		MAGENTA: Tuple[int, int, int] = 255,   0, 255
-		YELLOW:  Tuple[int, int, int] = 255, 255,   0
+		RED:   Colour = 255,   0,   0
+		GREEN: Colour =   0, 255,   0
+		BLUE:  Colour =   0,   0, 255
+
+		CYAN:    Colour =   0, 255, 255
+		MAGENTA: Colour = 255,   0, 255
+		YELLOW:  Colour = 255, 255,   0
+
+		class flat_ui:
+			class gb:
+				PROTOSS_PYLON: Colour =   0, 168, 255
+				VANADYL_BLUE:  Colour = 156, 136, 255
+
+				NASTURCIAN_FLOWER:      Colour = 232, 65, 24
+				HARLEY_DAVIDSON_ORANGE: Colour = 194, 54, 22
+
+				PERIWINKLE:  Colour = 156, 136, 255
+				MATT_PURPLE: Colour = 140, 122, 230
+
+				LYNX_WHITE:      Colour = 245, 246, 250
+				HINT_OF_PENSIVE: Colour = 220, 221, 225
+
+				RISE_N_SHINE:     Colour = 251, 197, 49
+				NANOHANACHA_GOLD: Colour = 225, 177, 44
+
+				BLUEBERRY_SODA:  Colour = 127, 143, 166
+				CHAIN_GANG_GREY: Colour = 113, 128, 147
+
+				DOWNLOAD_PROGRESS: Colour = 76, 209, 55
+				SKIRRET_GREEN:     Colour = 68, 189, 50
+
+				MAZARINE_BLUE: Colour = 39, 60, 117
+				PICO_VOID:     Colour = 25, 42,  86
+
+				SEABROOK: Colour = 72, 126, 176
+				NAVAL:    Colour = 64, 115, 158
+
+				BLUE_NIGHTS:     Colour = 53, 59, 72
+				ELECTROMAGNETIC: Colour = 47, 54, 64
 
 	WIDTH: int = 1280
 	HEIGHT: int = 720
