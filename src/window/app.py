f68e8f168e9d7f9d027689f1db9c1cc6a9c88045
moddedTechnic <jpleeming51@gmail.com>
2020-08-03 15:15:52 +0100

    Add new buttons to app for testing

--- src/window/app.py
+++ src/window/app.py
@@ -1,12 +1,13 @@
 from pygame import (
 	init as pg_init,
 	Surface,
-	QUIT as pg_QUIT,
-	quit as pg_quit
+	quit as pg_quit,
+	QUIT as QUIT_EVENT,
+	MOUSEBUTTONDOWN as MOUSEBUTTONDOWN_EVENT, MOUSEBUTTONUP as MOUSEBUTTONUP_EVENT,
 )
 from pygame.display import (
 	set_mode, set_caption,
-	update as update_display
+	update as update_display,
 )
 from pygame.event import (
 	get as get_events,
@@ -16,7 +17,14 @@ from pygame.time import (
 	Clock,
 )
 
+from typing import Dict
+
+
+from .components import Button, ToggleButton, CheckBox
 from .constants import constants
+from .event import Event
+
+colour = constants.colour.flat_ui.gb
 
 
 pg_init()
@@ -29,21 +37,53 @@ class App:
 
 	def __init__(self):
 		self.surface: Surface = set_mode((constants.WIDTH, constants.HEIGHT))
-
 		self.clock = Clock()
-		
 		self.running = True
+
+		self.buttons = []
+		self.buttons.append( Button(
+			self.surface,
+			(100, 50, 100, 50),
+			colour.SKIRRET_GREEN, colour.NASTURCIAN_FLOWER,
+			lambda: print('Button'),
+			'Button'
+		) )
+		self.buttons.append( ToggleButton(
+			self.surface,
+			(50, 150, 200, 50),
+			colour.SKIRRET_GREEN, colour.NASTURCIAN_FLOWER,
+			lambda state: print(f'Toggle Button: {state}'),
+			'Toggle Button'
+		) )
+		self.buttons.append( CheckBox(
+			self.surface,
+			(50, 250, 50, 50),
+			lambda state: print(f'Check Box: {state}')
+		) )
 
 	def __call__(self):
 		while self.running:
 			for e in get_events():
 				event: EventType = e
-				if event.type == pg_QUIT:
+				if event.type == QUIT_EVENT:
 					self.running = False
+				else:
+					self.dispatch(event.type, event.dict)
 
-			self.surface.fill(constants.colour.LIGHT_GREY)
+			self.surface.fill(colour.BLUE_NIGHTS)
+
+			[b.render() for b in self.buttons]
 
 			update_display()
 			self.clock.tick(60)
 
 		pg_quit()
+
+	def dispatch(self, event_type: Event, event_data: Dict):
+		if event_type in (MOUSEBUTTONDOWN_EVENT, MOUSEBUTTONUP_EVENT,):
+			direction = 0 if event_type == MOUSEBUTTONUP_EVENT else 1
+			x, y = event_data['pos']
+			button = event_data['button']
+			
+			if button == 1:
+				[b(x, y, direction) for b in self.buttons]
