189e8a0345c871067554e5d53b4875186b14d3e4
moddedTechnic <jpleeming51@gmail.com>
2020-08-03 20:24:22 +0100

    BugFix: Moved ComponentBase to fix circular import

--- src/window/components/__init__.py
+++ src/window/components/__init__.py
@@ -1,4 +1,22 @@
+
+class ComponentBase:
+	def __init__(self, rect):
+		self._name = 'Component'
+
+		self.surface = None
+		
+		from pygame import Rect
+		self.rect = Rect(rect)
+
+	def __str__(self):
+		return f'{self._name} ( [{self.rect.x}, {self.rect.y}], [{self.rect.width}, {self.rect.height}] )'
+
+	def mount(self, surface):
+		self.surface = surface
+
+	def render(self):
+		raise NotImplementedError('Components must implement a render method')
+
+
 from . import button
 from . import slider
-
-from .component import ComponentBase
